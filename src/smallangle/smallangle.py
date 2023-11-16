71b80e5e1e2b52ad44047ff8cd263f151f2061c5
pleundehaan <135601079+pleundehaan@users.noreply.github.com>
2023-11-16 11:26:42 +0100

    made a group with steps and sin/tan you can choose

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,9 +1,24 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+# create a group to create different subcomments
+@click.group()
+def geometric_group():
+    pass
+
+
+@geometric_group.command()
+# number is the number of steps between 0 and 2 pi
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Number of times to print greeting.",
+    show_default=True,  # show default in help
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
@@ -11,6 +26,15 @@ def sin(number):
     return
 
 
+@geometric_group.command()
+# number is the number of steps between 0 and 2 pi
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Number of times to print greeting.",
+    show_default=True,  # show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +43,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    geometric_group()
